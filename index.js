289498bc2636a75c327111fb749055ce673e47c0
lanell1 <lanelllopez@pursuit.org>
2023-12-09 08:35:11 -0500

    01-06-javascript-objects-practice

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// let person = {};
+// person.firstName = `Alice`;
+// person.lastName = `Smith`;
+
+// console.log('Full name: person.firstName + ' ' + person.lastName')
+
+let person = {};
+person.firstName = 'Alice';
+person.lastName = 'Smith';
+
+// Corrected console.log statement
+console.log('Full name:', person.firstName + ' ' + person.lastName);
